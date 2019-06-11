76ae174cb29c819ce4c2917266b85211b9f8f2ff
王小明 <wangxiaoming@wangxiaomingdeMacBook-Air.local>
2019-06-11 12:04:33 +0800

    Daily Update

---
+++ easy/112_路径总和_path-sum.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        
+        sum -= root.val;
+        if ((root.left == null) && (root.right == null)) return (sum == 0);
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
+}
